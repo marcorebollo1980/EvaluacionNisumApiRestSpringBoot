7900236de435b03edea7a1b1a2005611b8273082
marcorebollo1980 <marcorebollolopez@gmail.com>
2023-09-21 09:34:34 -0500

    commit firts
    Creacion de la estructura inicial del proyecto en SpringBoot

---
+++ usuarioregister/src/main/java/com/creation/usuario/UsuarioregisterApplication.java
@@ -0,0 +1,13 @@
+package com.creation.usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioregisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioregisterApplication.class, args);
+	}
+
+}
